f9083ad2d384a2741b3152bf74e20a887cde49d8
serefkilic <seref.kilic@testinium.com>
2026-01-05 15:58:29 +0300

    edit .sh

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -6,9 +6,9 @@ echo "==> Running k6 GraphQL"
 k6 run k6/graphql/rickmorty-characters.js || true
 
 echo "==> Setting up Python environment"
-python -m pip install -U pip
-pip install -r playwright-python/requirements.txt
-python -m playwright install --with-deps chromium
+python3 -m pip install -U pip
+pip3 install -r playwright-python/requirements.txt
+python3 -m playwright install --with-deps chromium
 
 echo "==> Running Playwright tests"
 pytest --headed --video on --tracing on
